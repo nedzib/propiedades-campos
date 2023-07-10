a616d0338c40260a913e7ed70df96393068a7d02
Nedzib Sastoque Rangel <nedzib.sastoque@gmail.com>
2023-07-10 11:42:48 -0500

    modules added

---
+++ src/app/modules/distribuidor/distribuidor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DistribuidorRoutingModule } from './distribuidor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DistribuidorRoutingModule
+  ]
+})
+export class DistribuidorModule { }
---
+++ src/app/modules/participante/participante.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ParticipanteRoutingModule } from './participante-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ParticipanteRoutingModule
+  ]
+})
+export class ParticipanteModule { }

06a786c2a5636694759e304742f214a4892ac460
Nedzib Sastoque <bsastoque@buk.co>
2023-12-11 11:38:00 -0500

    refactor: replacing miu for mu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,7 +34,7 @@ export class AppComponent {
   tdp = 0
 
   epsilon_0 = Math.pow(10, -9) / (36 * Math.PI)
-  miu_0 = 1.256637061e-6
+  mu_0 = 1.256637061e-6
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -42,7 +42,7 @@ export class AppComponent {
     alpha: 3.183,
     beta: 5.183,
     epsilon: 2.0937671721895996,
-    miu: 1.22,
+    mu: 1.22,
     f: 122200000,
     sigma: 0.028030256653737033,
   });
@@ -53,13 +53,13 @@ export class AppComponent {
     let alpha : any = this.valuesForm.value.alpha
     let beta : any = this.valuesForm.value.beta
     let f : any = this.valuesForm.value.f
-    let mu_r : any = this.valuesForm.value.miu
+    let mu_r : any = this.valuesForm.value.mu
 
     let mu = 0
     let epsilon = 0
 
     if (mu_r != null){
-      mu = mu_r * this.miu_0
+      mu = mu_r * this.mu_0
     }
 
     if (epsilon_r != null){
@@ -80,7 +80,7 @@ export class AppComponent {
         ' \\\\ \\frac{\\epsilon}{\\epsilon_{0}} = \\epsilon_{r} =' + epsilon / this.epsilon_0
       mu = Math.sqrt((4*Math.pow(alpha, 2)*Math.pow(beta, 2)*(Math.pow(alpha, 2)+Math.pow(beta, 2)))/(Math.pow(sigma, 2)*Math.pow(this.w, 2)*(Math.pow(alpha, 2)+Math.pow(beta, 2)+Math.pow(sigma, 2))))
       this.despeje_2_formula = '\\mu = \\sqrt{\\frac{4\\alpha^2\\beta^2(\\alpha^2+\\beta^2)}{\\sigma^2\\omega^2(\\alpha^2+\\beta^2)}} = ' + mu +
-        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.miu_0
+        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.mu_0
     }
 
     if (beta == null && epsilon_r == null){
@@ -105,9 +105,9 @@ export class AppComponent {
 
       mu = (2 * alpha * alpha) / ((this.w * this.w * this.epsilon_0 * epsilon_r) * (Math.sqrt(1 + (this.tdp * this.tdp)) - 1))
       this.despeje_1_formula = '\\mu = \\frac{2 \\alpha^2}{\\omega^2\\epsilon[\\sqrt{1+(\\frac{\\sigma}{\\omega\\epsilon})^2}-1]} = ' + mu +
-        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.miu_0
+        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.mu_0
 
-      mu_r = mu / this.miu_0
+      mu_r = mu / this.mu_0
 
       let solucion_beta = this.calcular_alpha_beta('beta', mu, epsilon, this.tdp)
       this.despeje_2_formula = solucion_beta.despeje
@@ -162,9 +162,9 @@ export class AppComponent {
 
       mu = (2 * beta * beta) / ((this.w * this.w * epsilon) * (Math.sqrt(1 + (this.tdp * this.tdp)) + 1))
       this.despeje_1_formula = '\\mu = \\frac{2 \\beta^2}{\\omega^2\\epsilon[\\sqrt{1+(\\frac{\\sigma}{\\omega\\epsilon})^2}+1]} = ' + mu +
-        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.miu_0
+        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.mu_0
 
-      mu_r = mu / this.miu_0
+      mu_r = mu / this.mu_0
 
       let solucion_alpha = this.calcular_alpha_beta('alpha', mu, epsilon, this.tdp)
       this.despeje_2_formula = solucion_alpha.despeje
@@ -196,7 +196,7 @@ export class AppComponent {
         '\\\\ \\sigma = \\omega*\\epsilon* \\sqrt{(\\frac{\\alpha^2+\\beta^2}{\\beta^2-\\alpha^2})^2-1} = ' + sigma
       mu = (2 * alpha * alpha) / ((this.w * this.w * epsilon) * (Math.sqrt(1 + (this.tdp * this.tdp)) - 1))
       this.despeje_2_formula = '\\mu = \\frac{2 \\alpha^2}{\\epsilon\\omega^2[\\sqrt{1+(\\frac{\\sigma}{\\omega\\epsilon})^2}-1]} = ' + mu +
-        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.miu_0
+        '\\\\ \\mu_{r} = \\frac{\\mu}{\\mu_{0}} = ' + mu / this.mu_0
 
     }
 
@@ -217,13 +217,13 @@ export class AppComponent {
     this.calcular_valores_eta(sigma, epsilon, mu)
   }
 
-  calcular_valores_eta(sigma: number, epsilon: number, miu: number) {
+  calcular_valores_eta(sigma: number, epsilon: number, mu: number) {
     let jw = math.multiply(math.complex('i'), this.w)
-    jw = math.multiply(jw, miu)
+    jw = math.multiply(jw, mu)
     const jwe = math.add(sigma, math.multiply(math.complex('i'), math.multiply(this.w, epsilon)))
     const added = math.divide(jw, jwe)
     this.eta = math.sqrt(added as math.Complex).toString()
-    this.norm_eta = Math.sqrt(miu / epsilon) / Math.pow(1 + Math.pow(sigma / (this.w * epsilon), 2), 1 / 4)
+    this.norm_eta = Math.sqrt(mu / epsilon) / Math.pow(1 + Math.pow(sigma / (this.w * epsilon), 2), 1 / 4)
     this.angle_eta_radians = Math.atan(sigma / (this.w * epsilon)) / 2
     this.angle_eta = this.angle_eta_radians * 180 / Math.PI
   }
